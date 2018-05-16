fd4985f74ffafc0d205b36a3fd529420761ca3e8
Shane Drosi <ShaneDrosi@Dobbie.local>
2018-05-15 17:26:37 -0700

    .gitignore

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Dobbie1123!",
+    password: "MySql12345!",
     database: "burgers_db",
     port: 3306
   });
